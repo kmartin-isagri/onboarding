713f2eb509c70b75f47fb0b35eb69888a1b472a7
kmartin-isagri <kmartin@groupeisagri.com>
2026-01-22 11:39:49 +0100

    exo 2 + 3

---
+++ training/src/components/ScoreToText.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ScoreToText">
+    <v-alert class="mb-6" text="Increment or decrement the score in exercice 2 to see the text change"></v-alert>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  scoring: {
+    type: Number,
+    required: true,
+  }
+})
+
+const text = computed(() => {
+  if (props.scoring % 3 === 0 && props.scoring % 5 === 0) {
+    return "Bobbee"
+  }
+
+  if (props.scoring % 3 === 0) {
+    return "Bob";
+  }
+
+  if (props.scoring % 5 === 0) {
+    return "bee";
+  }
+
+  return props.scoring
+})
+</script>
--- training/src/views/HomeView.vue
+++ training/src/views/HomeView.vue
@@ -8,7 +8,13 @@
 
     <v-card class="exo-card" title="Exercice 2" elevation="0">
       <v-card-text class="px-6 pt-6 pb-10">
-        <CharCounter />
+        <ScoreCounter v-model="scoring" />
+      </v-card-text>
+    </v-card>
+
+    <v-card class="exo-card" title="Exercice 3" elevation="0">
+      <v-card-text class="px-6 pt-6 pb-10">
+        <ScoreToText :scoring="scoring" />
       </v-card-text>
     </v-card>
   </div>
@@ -16,6 +22,11 @@
 
 <script lang="ts" setup>
 import CharCounter from '@/components/CharCounter.vue';
+import ScoreCounter from '@/components/ScoreCounter.vue';
+import ScoreToText from '@/components/ScoreToText.vue';
+import { ref } from 'vue';
+
+const scoring = ref(0)
 
 </script>
 
